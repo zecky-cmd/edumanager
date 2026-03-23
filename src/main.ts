b9afdde80da1e89d0e3e1172b445c4a55b4b6dd5
EzéchielDev <yaoezchiel@gmail.com>
2026-03-23 16:55:21 +0000

    desynchronisation entre les packages

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT || 8080);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
   console.log(`Swagger documentation available at: ${await app.getUrl()}/api/docs`);
 }
